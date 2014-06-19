3cc3bab6f1f3a7dcd2c76fd35259bdbb89601f19
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-06-19 22:52:33 +0100

    removed unused dependencies fixed livereload firing too soon

--- gulpFile.js
+++ gulpFile.js
@@ -11,7 +11,6 @@ var EXPRESS_PORT = 4000;
 
 
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 var watchify = require('watchify');
 var concat = require('gulp-concat');
 var minifyCSS = require('gulp-minify-css');
@@ -23,29 +22,16 @@ var livereload = require('gulp-livereload');
 var notify = require("gulp-notify");
 var uglify = require('gulp-uglify');
 var jsValidate = require('gulp-jsvalidate');
-var yuidoc = require("gulp-yuidoc");
-gulp.on('err', function(err){
-	console.log("on general err");
-	  console.log(err);
-});
-
-
-var log = function(mainMsg, secondaryMsg) {
-	var args = ['[' + gutil.colors.green(new Date().toLocaleTimeString()) + ']'];
-	if (mainMsg) args.push(gutil.colors.cyan(mainMsg));
-	if (secondaryMsg) args.push(gutil.colors.magenta(secondaryMsg));
-	gutil.log.apply(null, args);
-};
 
 
 gulp.task('concatCss', function() {
-  gulp.src(paths.style)
+  return gulp.src(paths.style)
   	.pipe(concat(outputName + '.css'))
     .pipe(gulp.dest(dest))
     ;
 });
 gulp.task('minifyCss', ['concatCss'], function() {
-	gulp.src(dest + "/" + outputName + ".css")
+	return gulp.src(dest + "/" + outputName + ".css")
 	.pipe(concat(outputName + '.min.css'))
     .pipe(minifyCSS())
 	.pipe(gulp.dest(dest))
@@ -54,14 +40,13 @@ gulp.task('minifyCss', ['concatCss'], function() {
 });
 
 gulp.task('connect', function() {
-//	log('Running on http://localhost:' + EXPRESS_PORT);
 	connect.server({
 		port : 4000,
 		livereload: true
 	});
 });
 gulp.task('browserify', function() {
-	gulp.src("./src/*.js").pipe(jsValidate()).on('error', 
+	return gulp.src("./src/*.js").pipe(jsValidate()).on('error', 
 		notify.onError({
 			message: "Error: <%= error.message %>",
 			title: "Failed running browserify"
@@ -78,11 +63,9 @@ gulp.task('browserify', function() {
 		    .pipe(gulp.dest(dest))
 		    .pipe(connect.reload());
 		});
-		
-		
 });
 gulp.task('minifyJs', function() {
-	gulp.src(dest + "/" + outputName + ".js")
+	return gulp.src(dest + "/" + outputName + ".js")
 	.pipe(concat(outputName + '.min.js'))
     .pipe(uglify())
 	.pipe(gulp.dest(dest));
@@ -101,7 +84,7 @@ gulp.task('watch', function() {
 
 
 gulp.task('makeDocLib', function() {
-	gulp.src("./doc/*.js").pipe(jsValidate()).on('error', 
+	return gulp.src("./doc/*.js").pipe(jsValidate()).on('error', 
 			notify.onError({
 				message: "Error: <%= error.message %>",
 				title: "Failed running browserify"
@@ -116,7 +99,7 @@ gulp.task('makeDocLib', function() {
 			});
 });
 gulp.task('makeDocCss', function() {
-	gulp.src(['node_modules/twitter-bootstrap-3.0.0/dist/css/bootstrap.css', './doc/main.css', 'node_modules/yasgui-yasqe/dist/yasqe.css'])
+	return gulp.src(['node_modules/twitter-bootstrap-3.0.0/dist/css/bootstrap.css', './doc/main.css', 'node_modules/yasgui-yasqe/dist/yasqe.css'])
   	.pipe(concat('bundles.css'))
     .pipe(gulp.dest("doc"))
     ;
@@ -124,7 +107,6 @@ gulp.task('makeDocCss', function() {
 });
 gulp.task('makedoc', ['makeDocLib', 'makeDocCss']);
 	
-gulp.task('packageMinified', ['minifyJs', 'minifyCss']);
-gulp.task('default', ['browserify', 'packageMinified']);
+gulp.task('default', ['browserify', 'minifyJs', 'minifyCss', 'makedoc']);
 gulp.task('serve', ['browserify', 'minifyCss', 'watch', 'connect']);
 
