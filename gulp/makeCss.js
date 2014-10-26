85f880ae0cee8c1ba3680bd7846a4d842336e761
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-10-26 23:43:41 +0100

    made gulp css file consistent with yasqe

--- gulp/makeCss.js
+++ gulp/makeCss.js
@@ -1,17 +1,17 @@
 var gulp = require('gulp'),
 	concat = require('gulp-concat'),
-	minifyCSS = require('gulp-minify-css'),
 	paths = require('./paths.js'),
-	connect = require('gulp-connect');
+	connect = require('gulp-connect'),
+	minifyCSS = require('gulp-minify-css');
 
 gulp.task('concatCss', function() {
-  gulp.src(paths.style)
+  return gulp.src(paths.style)
   	.pipe(concat(paths.bundleName + '.css'))
     .pipe(gulp.dest(paths.bundleDir))
     ;
 });
 gulp.task('minifyCss', ['concatCss'], function() {
-	gulp.src(paths.bundleDir + "/" + paths.bundleName + ".css")
+	return gulp.src(paths.bundleDir + "/" + paths.bundleName + ".css")
 	.pipe(concat(paths.bundleName + '.min.css'))
     .pipe(minifyCSS())
 	.pipe(gulp.dest(paths.bundleDir))
