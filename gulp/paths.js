1080bb06219343ed7bdcc6c355cb0204bf150354
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-10-31 22:08:08 +0100

    added missing codemirror styles to lib

--- gulp/paths.js
+++ gulp/paths.js
@@ -2,6 +2,7 @@ module.exports =  {
 	style: [
 		'lib/DataTables/media/css/jquery.dataTables.css',
 		'jquery.dataTables_themeroller.css',
+		'node_modules/codemirror/lib/codemirror.css', 
 		'src/**/*.scss',
 	],
 	bundleDir: "dist",
