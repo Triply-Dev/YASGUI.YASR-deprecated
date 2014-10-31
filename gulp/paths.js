64c6f95556330a6d12f7236edfc7ff65b4fe5a52
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-10-31 20:36:49 +0100

    use sass instead of css

--- gulp/paths.js
+++ gulp/paths.js
@@ -2,7 +2,7 @@ module.exports =  {
 	style: [
 		'lib/DataTables/media/css/jquery.dataTables.css',
 		'jquery.dataTables_themeroller.css',
-		'src/**/*.css',
+		'src/**/*.scss',
 	],
 	bundleDir: "dist",
 	bundleName: "yasr"
