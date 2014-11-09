ecea1b909a51b95c7aaa3d4bad40fe9c2ce0ba51
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-11-09 17:15:29 +0100

    switch to jsdelivr, and use their api to show CDN info

--- doc/main.js
+++ doc/main.js
@@ -18,3 +18,26 @@ $(function() {
     }
   });
 });
+
+
+$(document).ready(function() {
+	//get the latest hosted version
+	if ($("#cdnDownload").length > 0) {
+		//only draw when we've got some place to print this info (might not be on all pages where we include this js file)
+		$.get("http://api.jsdelivr.com/v1/jsdelivr/libraries?name=yasr&fields=lastversion", function(data) {
+			if (data.length > 0) {
+				var version = data[0].lastversion;
+				$("#yasrCss").text("<link href='//cdn.jsdelivr.net/yasr/" + version + "/yasr.min.css' rel='stylesheet' type='text/css'/>");
+				$("#yasrJsBundled").text("<script src='//cdn.jsdelivr.net/yasr/" + version + "/yasr.bundled.min.js'></script" + ">");
+				$("#yasrJs").text("<script src='//cdn.jsdelivr.net/yasr/" + version + "/yasr.min.js'></script" + ">");
+			} else {
+				console.log("failed accessing jsdelivr api");
+				$("#cdnDownload").hide();
+			}
+		}).fail(function() {
+			console.log("failed accessing jsdelivr api");
+			$("#cdnDownload").hide();
+		});
+	}
+});
+
