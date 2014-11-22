9803413803870234c2b377cb9a447e0377790cfc
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-11-22 17:00:39 +0100

    support folding/unfolding of xml and json

--- gulp/paths.js
+++ gulp/paths.js
@@ -3,6 +3,7 @@ module.exports =  {
 		'lib/DataTables/media/css/jquery.dataTables.css',
 		'jquery.dataTables_themeroller.css',
 		'node_modules/codemirror/lib/codemirror.css', 
+		'node_modules/codemirror/addon/fold/foldgutter.css', 
 		'src/**/*.scss',
 	],
 	bundleDir: "dist",
--- src/rawResponse.js
+++ src/rawResponse.js
@@ -1,6 +1,11 @@
 var $ = require("jquery"),
 	CodeMirror = require("codemirror");
 
+require('codemirror/addon/fold/foldcode.js');
+require('codemirror/addon/fold/foldgutter.js');
+require('codemirror/addon/fold/xml-fold.js');
+require('codemirror/addon/fold/brace-fold.js');
+
 require('codemirror/addon/edit/matchbrackets.js');
 require('codemirror/mode/xml/xml.js');
 require('codemirror/mode/javascript/javascript.js');
@@ -8,6 +13,7 @@ require('codemirror/mode/javascript/javascript.js');
 var root = module.exports = function(yasr) {
 	var plugin = {};
 	var options = $.extend(true, {}, root.defaults);
+	var cm = null;
 	var draw = function() {
 		var cmOptions = options.CodeMirror;
 		cmOptions.value = yasr.results.getOriginalResponseAsString();
@@ -20,7 +26,16 @@ var root = module.exports = function(yasr) {
 			cmOptions.mode = mode;
 		}
 		
-		CodeMirror(yasr.resultsContainer.get()[0], cmOptions);
+		cm = CodeMirror(yasr.resultsContainer.get()[0], cmOptions);
+		
+		//CM has some issues with folding and unfolding (blank parts in the codemirror area, which are only filled after clicking it)
+		//so, refresh cm after folding/unfolding
+		cm.on('fold', function() {
+			cm.refresh();
+		});
+		cm.on('unfold', function() {
+			cm.refresh();
+		});
 		
 	};
 	var canHandleResults = function(){
@@ -57,6 +72,10 @@ var root = module.exports = function(yasr) {
 root.defaults = {
 	CodeMirror: {
 		readOnly: true,
+		lineNumbers: true,
+	    lineWrapping: true,
+	    foldGutter: true,
+	    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
 	}
 };
 
