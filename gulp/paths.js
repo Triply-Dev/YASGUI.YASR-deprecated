42fcccff7ad66acc06aa3a7dac20ae2dfa126991
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-11-25 12:01:19 +0100

    moved logic of style concatenation from gulp to sass, to better support re-use in other components

--- gulp/paths.js
+++ gulp/paths.js
@@ -1,11 +1,6 @@
 module.exports =  {
-	style: [
-		'lib/DataTables/media/css/jquery.dataTables.css',
-		'jquery.dataTables_themeroller.css',
-		'node_modules/codemirror/lib/codemirror.css', 
-		'node_modules/codemirror/addon/fold/foldgutter.css', 
-		'src/**/*.scss',
-	],
+//	style: ['src/scss/scoped.scss'],
+	style: ['src/scss/scoped.scss', 'src/scss/global.scss'],
 	bundleDir: "dist",
 	bundleName: "yasr"
 };
