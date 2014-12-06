6c38067355616be7050be2baeec883685ee7d37e
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-12-06 15:50:47 +0100

    use local yasqe lib in dev, and use cdn version when publishing release

--- gulpFile.js
+++ gulpFile.js
@@ -1,6 +1,6 @@
 require('require-dir')('./gulp');
 
 var gulp = require('gulp');	
-gulp.task('default', ['browserify', 'browserifyWithDeps', 'makeCss', 'makeMainPage']);
-gulp.task('serve', ['makeCss', 'makeMainPage', 'browserifyForDebug', 'watch', 'connect']);
+gulp.task('default', ['modifyYasqeInclude', 'browserify', 'browserifyWithDeps', 'makeCss', 'makeMainPage']);
+gulp.task('serve', ['modifyYasqeIncludeDev', 'makeCss', 'makeMainPage', 'browserifyForDebug', 'watch', 'connect']);
 
