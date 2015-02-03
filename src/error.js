61cd22c59a54f41a0dfa922834ad0f8ecbe4fdbb
Laurens Rietveld <laurens.rietveld@gmail.com>
2015-02-03 09:10:45 +0100

    some styling improvements related to exception handling (YASGUI/YASR#44)

--- src/error.js
+++ src/error.js
@@ -19,29 +19,33 @@ var root = module.exports = function(yasr) {
 		var error = yasr.results.getException();
 		$container.empty().appendTo(yasr.resultsContainer);
 
-		var statusText = 'Error';
-		if (error.statusText && error.statusText.length < 100) {
-			//use a max: otherwise the alert span will look ugly
-			statusText = error.statusText;
-		}
-		if (error.status != undefined) {
+		
+		if (error.status !== 0) {
+			var statusText = 'Error';
+			if (error.statusText && error.statusText.length < 100) {
+				//use a max: otherwise the alert span will look ugly
+				statusText = error.statusText;
+			}
+			
+			
 			statusText += ' (#' + error.status + ')';
-		}
-		$container
+			
+			$container
 			.append(
 				$("<span>", {class:'exception'})
 				.text(statusText)
 			);
-		var responseText = null;
-		if (error.responseText) {
-			responseText = error.responseText;
-		} else if (typeof error == "string") {
-			//for backwards compatability (when creating the error string was done externally
-			responseText = error;
-		}
-		if (responseText) {
-			$container.append($("<pre>", {class:'corsMessage'}).text(responseText));
-		} else if (error.status === 0){
+			
+			var responseText = null;
+			if (error.responseText) {
+				responseText = error.responseText;
+			} else if (typeof error == "string") {
+				//for backwards compatability (when creating the error string was done externally
+				responseText = error;
+			}
+			if (responseText) $container.append($("<pre>").text(responseText));
+		} else {
+		
 			//cors disabled, wrong url, or endpoint down
 			$container.append(
 				$('<div>', {class: 'corsMessage'})
