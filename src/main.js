6695316812407f08a9d6fd61ae50f851b3eb6d4e
Laurens Rietveld <laurens.rietveld@gmail.com>
2017-12-07 11:15:32 +0100

    added fullscreen event emission

--- src/main.js
+++ src/main.js
@@ -295,6 +295,10 @@ var YASR = function(parent, options, queryResults) {
           yasr.container.addClass("yasr_fullscreen");
           //draw, as yasr dimensions have changed (needed for e.g. leaflet)
           yasr.draw();
+          //useful to emit this, as then we can e.g. update the yasgui style when this happens
+          //needed this to fix z-index issues with leaflet, as the yasgui el has a relative pos
+          yasr.emit('fullscreen-enter', yasr);
+
         });
       yasr.header.append(button);
     };
@@ -305,6 +309,7 @@ var YASR = function(parent, options, queryResults) {
           yasr.container.removeClass("yasr_fullscreen");
           //draw, as yasr dimensions have changed (needed for e.g. leaflet)
           yasr.draw();
+          yasr.emit('fullscreen-leave', yasr);
         });
       yasr.header.append(button);
     };
